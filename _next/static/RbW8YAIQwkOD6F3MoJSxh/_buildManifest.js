488e515eefb9aaa1237e90f870bf0602bb440fb6
Mika Siddiqui <36973525+mikasiddiqui@users.noreply.github.com>
2026-02-01 05:38:43 +1100

    Updates

--- _next/static/RbW8YAIQwkOD6F3MoJSxh/_buildManifest.js
+++ _next/static/RbW8YAIQwkOD6F3MoJSxh/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/wedding//_next/:path+",
-        "destination": "/wedding/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
